a197e820b4bb57bdd63cea8a966601626df041d3
Lincoln Glauser <ldg4627@rit.edu>
2016-05-02 21:16:59 -0400

    Hello World

---
+++ Mini-Games/Mini-Games/Mini-Games.cpp
@@ -0,0 +1,14 @@
+// Mini-Games.cpp : Defines the entry point for the console application.
+//
+
+
+#include <cstdio>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Hello World" << endl;
+    return 0;
+}
+
