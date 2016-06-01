43746efd4a9620dadd3b4e161b1e80cc4d957fd6
Lincoln Glauser <ldg4627@rit.edu>
2016-06-01 00:16:30 -0600

    Highscorefile sorted and replaces on a per name basis

--- Mini-Games/Include/HighScores.h
+++ Mini-Games/Include/HighScores.h
@@ -9,6 +9,8 @@
 #include <fstream>
 #include <cctype>
 #include <sstream>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
@@ -18,6 +20,10 @@ struct highscoreRecord {
 	int gameID;
 	char name[30];
 	int numTurns;
+
+	bool operator<(const highscoreRecord& val) const {
+		return numTurns < val.numTurns;
+	}
 };
 
 
@@ -31,14 +37,17 @@ public:
 	void write_file();
 	bool read_file(int);
 	int search_open_index();
-	int search_by_name(string, int);
-	//void delete_file();
+	highscoreRecord search_by_name(string, int);
+	bool replace(string, int, int);
+	void delete_score();
 	void display_all(int);
+	void sort();
 	void display_data();
 	/*void clear_stream();
 	*/
 private:
 	highscoreRecord highscores;
+	vector<vector<highscoreRecord>> vHighscores;
 	fstream highscoreFile;
 };
 
--- Mini-Games/Source/HighScores.cpp
+++ Mini-Games/Source/HighScores.cpp
@@ -50,7 +50,7 @@ int HighScores::search_open_index()
 	return 0;
 }
 
-int HighScores::search_by_name(string name, int gID)
+highscoreRecord HighScores::search_by_name(string name, int gID)
 {
 	char tmp[30];
 	if (name.length() < 30) {
@@ -70,33 +70,35 @@ int HighScores::search_by_name(string name, int gID)
 		highscoreFile.seekg((num - 1) * sizeof(highscores));
 		highscoreFile.read(reinterpret_cast<char *> (&highscores), sizeof(highscores));
 		if ((strcmp(tmp,highscores.name) == 0) && (gID == highscores.gameID)) {
-			return highscores.numTurns;
+			return highscores;
 		}
 	}
-	cout << "Name not found in file (NO HIGHSCORE)" << endl;
-	return 0;
+	highscores.idnum = 0;
+	return highscores;
 }
 
 // function to read the input data from the user
 bool HighScores::input(string name, int gID, int nTurns)
 {
-	highscores.idnum = search_open_index();
-	if (name.length() < 30) {
-		for (int i = 0; i < name.length(); i++) {
-			highscores.name[i] = name[i];
+	if (!replace(name, gID, nTurns)) {
+		highscores.idnum = search_open_index();
+		if (name.length() < 30) {
+			for (int i = 0; i < name.length(); i++) {
+				highscores.name[i] = name[i];
+			}
+			highscores.name[name.length()] = '\0';
 		}
-		highscores.name[name.length()] = '\0';
-	}
-	else
-	{
-		for (int i = 0; i < 29; i++) {
-			highscores.name[i] = name[i];
+		else
+		{
+			for (int i = 0; i < 29; i++) {
+				highscores.name[i] = name[i];
+			}
+			highscores.name[29] = '\0';
 		}
-		highscores.name[29] = '\0';
+		highscores.gameID = gID;
+		highscores.numTurns = nTurns;
+		write_file();
 	}
-	highscores.gameID = gID;
-	highscores.numTurns = nTurns;
-	write_file();
 	return true;
 }
 
@@ -104,6 +106,14 @@ bool HighScores::input(string name, int gID, int nTurns)
 void HighScores::write_file()
 {
 	highscoreFile.seekp((highscores.idnum - 1) * sizeof(highscores));
+	highscoreFile.write(reinterpret_cast<char *> (&highscores), sizeof(highscores));
+}
+
+void HighScores::delete_score()
+{
+	int temp = highscores.idnum;
+	highscores.idnum = 0;
+	highscoreFile.seekp((temp - 1) * sizeof(highscores));
 	highscoreFile.write(reinterpret_cast<char *> (&highscores), sizeof(highscores));
 }
 
@@ -122,8 +132,22 @@ bool HighScores::read_file(int num)
 	}
 }
 
+bool HighScores::replace(string name, int gID, int numTurns)
+{
+	if ((search_by_name(name, gID).numTurns > numTurns) && (search_by_name(name, gID).idnum > 0)) {
+		highscores = search_by_name(name, gID);
+		highscores.numTurns = numTurns;
+		write_file();
+		return true;
+	} 
+	else {
+		return false;
+	}
+}
+
 void HighScores::display_all(int gID)
 {
+	sort();
 	cout << left
 		<< setw(30) << "Name"
 		<< setw(7) << "# Turns" << endl;
@@ -140,6 +164,40 @@ void HighScores::display_all(int gID)
 				<< highscores.numTurns << endl;
 		}
 	}
+}
+
+void HighScores::sort()
+{
+// Current Supports gID 0 & 1
+	// Setup Vector
+	vHighscores.resize(2);
+	// Grab all gID highscores and sort 
+	for (int gID = 0; gID < 2; gID++) {
+		for (int num = 1; num < 100; num++) {
+			highscoreFile.seekg((num - 1) * sizeof(highscores));
+			highscoreFile.read(reinterpret_cast<char *> (&highscores), sizeof(highscores));
+			if (num != highscores.idnum) {
+				break;
+			}
+			else if (highscores.gameID == gID) {
+				vHighscores[gID].push_back(highscores);
+				// data is now "obsolete" from file
+				delete_score();
+			}
+		}
+		// Sort gID highscores
+		std::sort(vHighscores[gID].begin(), vHighscores[gID].end());
+	}
+	int ID = 1;
+	for (int gID = 0; gID < 2; gID++) {
+		for (int num = 0; num < vHighscores[gID].size(); num++) {
+			highscores = vHighscores[gID][num];
+			highscores.idnum = ID;
+			write_file();
+			ID++;
+		}
+	}
+	vHighscores.resize(0);
 }
 
 // function to display results to the screen
